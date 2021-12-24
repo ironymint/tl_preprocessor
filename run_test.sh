d28e9b5c0db959ab3b12776187177617f8b65b1b
Juwan Lee <jl@Juwanui-MacBookPro.local>
2021-12-24 17:13:04 +0900

    add option

--- run_test.sh
+++
@@ -1,4 +0,0 @@
-python tl_main.py --path ./dataset/ --aug NO > no_aug.txt
-mv ./dataset/test_classified ./dataset/test_classified_no
-python tl_main.py --path ./dataset/ --aug RANDOM > rand_aug.txt
-mv ./dataset/test_classified ./dataset/test_classified_rand
